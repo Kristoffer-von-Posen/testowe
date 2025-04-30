e5411327a8f0aff2d679624b9016ef502135b253
Kristoffer-von-Posen <krzysztof.chanski@gmail.com>
2025-04-30 23:14:09 +0200

    Update settings.py

--- proba/settings.py
+++ proba/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-import socket """android shit baby"""
+import socket
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
